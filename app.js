e9ea46a2a4ba617c21cac5542ae7eb06c0dd1166
Kalid Diriye <45129382+KD77@users.noreply.github.com>
2021-05-15 22:28:30 +0200

    connected to the database

---
+++ app.js
@@ -0,0 +1,18 @@
+const express= require('express')
+const mongoose= require('mongoose')
+require('dotenv/config')
+
+const app = express()
+
+const PORT= 3000
+
+mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true, useUnifiedTopology: true })
+const con= mongoose.connection;
+con.on('open', () =>{
+  console.log('Connected')
+})
+
+
+app.listen(PORT,()=>{
+  console.log('Server started')
+})
